0bb7fd1438d7d842fea3ce44e2f0594e13b7ddd7
ShaharD25 <shahar_d25@hotmail.com>
2025-04-25 02:19:36 +0300

    Remove misplaced src folder from root directory

--- src/routes/transactionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const transactionController = require('../controllers/transactionController');
-
-// PATCH /transactions/:id/status – update transaction status
-router.patch('/:id/status', transactionController.updateTransactionStatus);
-
-module.exports = router;
