6d2645466b25ce0b3476094da30cce387934b8ba
ShaharD25 <shahar_d25@hotmail.com>
2025-04-25 02:35:19 +0300

    Fix: add FinanceService as a regular folder, not submodule

---
+++ Backend/FinanceService/src/data-access/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DB_CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = pool;
