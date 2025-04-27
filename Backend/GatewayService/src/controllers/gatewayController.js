476a2fa36fbd861446f282e5a60f7f9df36a2fcb
Merylhassid <157501246+Merylhassid@users.noreply.github.com>
2025-04-27 12:02:51 +0300

    Update gatewayController.js

--- Backend/GatewayService/src/controllers/gatewayController.js
+++ Backend/GatewayService/src/controllers/gatewayController.js
@@ -30,6 +30,37 @@ const forwardAuthRequests = async (req, res, next) => {
     return next(error);
   }
 };
+const forwardFinanceRequests = async (req, res, next) => {
+  try {
+    const financeServiceUrl = process.env.FINANCE_SERVICE_URL;  
+    const path = req.originalUrl.replace('/finance', '');   
+    const url = `${financeServiceUrl}${path}`;
+
+    console.log(`Forwarding request to ${url}`, ` body: ${req.body}`);
+
+    // Forward the exact method and body
+    const response = await axios({
+      method: req.method,
+      url,
+      data: req.body,
+    });
+
+    console.log('response: ', response.data);
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    // Error from the microservice or network
+    console.log(
+      'Error while forwarding request to finance service. Error: ',
+      error,
+      error?.data
+    );
+
+    if (error.response) {
+      return res.status(error.response.status).json(error.response.data);
+    }
+    return next(error);
+  }
+};
 
 export async function ping(req, res, next) {
   try {
@@ -39,4 +70,4 @@ export async function ping(req, res, next) {
   }
 }
 
-export { forwardAuthRequests };
+export { forwardAuthRequests, forwardFinanceRequests };
