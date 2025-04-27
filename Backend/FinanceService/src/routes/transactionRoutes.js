f012b58e5b3070207c7f33b64780461ef69c431e
yaelberko <141316555+yaelberko@users.noreply.github.com>
2025-04-27 14:19:13 +0300

    Update transactionRoutes.js

--- Backend/FinanceService/src/routes/transactionRoutes.js
+++ Backend/FinanceService/src/routes/transactionRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { updateTransactionStatus } = require('../controllers/transactionController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../../middleware/authMiddleware');
 
 router.patch('/:id', authMiddleware, updateTransactionStatus);
 
