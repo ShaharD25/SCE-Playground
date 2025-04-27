5cafec1c7475cb09826e36d114e41c7afd1a0e3e
Merylhassid <157501246+Merylhassid@users.noreply.github.com>
2025-04-27 18:58:26 +0300

    Update gatewayRoutes.js

--- Backend/GatewayService/src/routes/gatewayRoutes.js
+++ Backend/GatewayService/src/routes/gatewayRoutes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import { forwardAuthRequests, forwardFinanceRequests, ping } from '../controllers/gatewayController.js';
 const router = Router();
 
-router.use('/finance', forwardFinanceRequests);  
 router.use('/auth', forwardAuthRequests);
+router.use('/finance', forwardFinanceRequests);  
 router.get('/ping', ping);
 
 export default router;
