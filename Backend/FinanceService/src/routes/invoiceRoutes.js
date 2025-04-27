d160e5e4dca7dbcd22c5eb219885fcccbef8202c
yaelberko <141316555+yaelberko@users.noreply.github.com>
2025-04-27 14:07:15 +0300

    Update invoiceRoutes.js

--- Backend/FinanceService/src/routes/invoiceRoutes.js
+++ Backend/FinanceService/src/routes/invoiceRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { createInvoice } = require('../controllers/invoiceController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../../middleware/authMiddleware');
 
 router.post('/', authMiddleware, createInvoice);
 
