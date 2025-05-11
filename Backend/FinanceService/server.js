90a1d2b1d1696999c92e2783c6013a1bfb865f60
ShaharD25 <shahar_d25@hotmail.com>
2025-05-12 01:13:00 +0300

    shahar update

--- Backend/FinanceService/server.js
+++ Backend/FinanceService/server.js
@@ -15,7 +15,7 @@ app.use('/invoices', invoiceRoutes);
 console.log('invoiceRoutes loaded');
 
 const transactionRoutes = require('./src/routes/transactionRoutes');
-app.use('/transactions', transactionRoutes);
+app.use('/finance/transactions', transactionRoutes);
 console.log('transactionRoutes loaded');
 
 const receiptRoutes = require('./src/routes/receiptRoutes');
