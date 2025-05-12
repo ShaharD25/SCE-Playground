6393eb4bccc0713c06e72c77565dff9aafab400b
ShaharD25 <shahar_d25@hotmail.com>
2025-05-13 02:31:48 +0300

    fix: correct express import

--- Backend/GatewayService/src/routes/gatewayRoutes.js
+++ Backend/GatewayService/src/routes/gatewayRoutes.js
@@ -1,4 +1,4 @@
-import { express} from 'express';
+import express from 'express';
 import { forwardAuthRequests, forwardFinanceRequests, ping } from '../controllers/gatewayController.js';
 const router = express.Router();
 
