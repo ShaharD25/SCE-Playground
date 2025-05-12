179b09bd28a6fa923ce7c167b6c7b52eb997db78
ShaharD25 <shahar_d25@hotmail.com>
2025-05-13 02:29:35 +0300

    fix: route gateway only for /finance

--- Backend/GatewayService/src/index.js
+++ Backend/GatewayService/src/index.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(express.json());
 
 
-app.use('/', gatewayRoutes);
+app.use('/finance', gatewayRoutes);
 
 
 app.use(errorHandler);
--- Backend/GatewayService/src/routes/gatewayRoutes.js
+++ Backend/GatewayService/src/routes/gatewayRoutes.js
@@ -1,6 +1,6 @@
-import { Router } from 'express';
+import { express} from 'express';
 import { forwardAuthRequests, forwardFinanceRequests, ping } from '../controllers/gatewayController.js';
-const router = Router();
+const router = express.Router();
 
 router.use('/auth', forwardAuthRequests);
 router.use('/finance', forwardFinanceRequests);  
