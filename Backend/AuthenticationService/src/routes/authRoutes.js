abcbfa936cceed7be6e967921994613c8a5bfb59
ShaharD25 <shahar_d25@hotmail.com>
2025-05-12 17:13:20 +0300

    ffff

--- Backend/AuthenticationService/src/routes/authRoutes.js
+++ Backend/AuthenticationService/src/routes/authRoutes.js
@@ -11,8 +11,11 @@ const router = Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin);
-router.post('/validate-token', validateToken);
-router.get('/ping', validateToken);
+//router.post('/validate-token', validateToken);
+//router.get('/ping', validateToken);
+router.post('/ping', (req, res) => res.status(200).json({ message: 'pong' }));
+router.get('/ping', (req, res) => res.status(200).json({ message: 'pong' }));
+
 router.delete('/user', deleteUser);
 
 export default router;
