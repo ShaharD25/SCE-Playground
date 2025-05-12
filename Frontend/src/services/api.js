b371e82e59c5ac71994c40af7b7b393f40153d43
ShaharD25 <shahar_d25@hotmail.com>
2025-05-12 03:58:10 +0300

    updat api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,9 +2,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: import.meta.env.VITE_GATEWAY_URL,
-  baseURL: 'https://sce-gateway.onrender.com',
+  baseURL: 'https://sce-gateway.onrender.com', // כתובת ה־Gateway בענן
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default api;
+
