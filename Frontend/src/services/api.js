099b430bc25005851cfb436566636c62dc06fd48
ShaharD25 <shahar_d25@hotmail.com>
2025-06-23 23:15:55 +0300

    finnal commit finance service

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_GATEWAY_BASE_URL || 'http://localhost:4000',
+  baseURL: import.meta.env.VITE_GATEWAY_URL || 'http://localhost:4000',
   withCredentials: true,
 });
 
